3e733a0c80441baae3796b30ff0f7c3ba6cd5a96
Adnan Vatric <vatric_adnan@hotmail.com>
2019-06-20 12:45:06 +0200

    add some magic

--- resources/views/practice.blade.php
+++ resources/views/practice.blade.php
@@ -13,17 +13,10 @@
                         <button type="button" id="target" class="btn btn-warning btn-lg btn-block mt-3">Weiter</button>
                         <div id="animal_alert" class="alert alert-danger alert-dismissible fade mt-3" role="alert">
                             <strong>Whoops!</strong> Try again!
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
                         </div>
                         <div id="animal_alert_pos" class="alert alert-success alert-dismissible fade mt-3" role="alert">
                             <strong>Great!</strong> Go on!
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -43,20 +36,26 @@
             $.post( "animal/check", { name: $('#animal_name').val(), id: $('#animal_id').val()}, function(data) {
                 if(data == 0){
                     $('#animal_alert').addClass('show');
-
                 }
                 else if (data == 1)
                 {
                     $('#animal_alert').removeClass('show');
+                    $('#animal_alert_pos').addClass('show');
 
-                    $.getJSON( "animal/get", function( data ) {
-                            $("#animal_text").text('This is ' + data.prefix);
-                            $('#animal_img').attr('src', 'img/' + data.filename);
-                            $('#animal_img').attr('style', 'width:' + data.width + 'px;height:' + data.height + 'px');
-                            $('#animal_card').attr('style', 'width:' + data.width + 'px;');
-                            $('#animal_id').val(data.id);
-                            $('#animal_name').val('');
-                        });
+                    setTimeout(
+                        function()
+                        {
+                            $('#animal_alert_pos').removeClass('show');
+
+                            $.getJSON( "animal/get", function( data ) {
+                                $("#animal_text").text('This is ' + data.prefix);
+                                $('#animal_img').attr('src', 'img/' + data.filename);
+                                $('#animal_img').attr('style', 'width:' + data.width + 'px;height:' + data.height + 'px');
+                                $('#animal_card').attr('style', 'width:' + data.width + 'px;');
+                                $('#animal_id').val(data.id);
+                                $('#animal_name').val('');
+                            });
+                        }, 3000);
                 }
             });
         });
